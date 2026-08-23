a50aa49587d8faa60855edac36d4c305e4a029c2
raquelwek <radavila@fi.uba.ar>
2026-08-23 20:12:28 -0300

    Revert "Revert "feat: ejercicio3 - client update to read the input file""
    This reverts commit 36ac7c7ee8376d668fd447ef3eaa7c55c77657e5.

--- services/client/src/main.go
+++ services/client/src/main.go
@@ -24,10 +24,16 @@ func loadConfig() (client.ClientConfig, error) {
 		return client.ClientConfig{}, errors.New("SERVER_PORT environment variable is required")
 	}
 
+	inputFile := os.Getenv("INPUT_FILE")
+	if inputFile == "" {
+		return client.ClientConfig{}, errors.New("INPUT_FILE environment variable is required")
+	}
+
 	return client.ClientConfig{
 		ServerHost: serverHost,
 		ServerPort: serverPort,
 		AgencyId:   agencyId,
+		InputFile:  inputFile,
 	}, nil
 }
 
