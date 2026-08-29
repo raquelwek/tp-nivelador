681d64ee887b15f734991b9af0bbe699a0e5d235
raquelwek <radavila@fi.uba.ar>
2026-08-29 15:03:49 -0300

    ejercicio 4: (server) recv all bytes

--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -1,11 +1,15 @@
 import socket
 
-# TODO: Complete with a short-read/short-write tolerant implementation
-
-
 def recv_all(socket: socket.socket, size):
-    return socket.recv(size)
+    bytes_received = b''
+    while len(bytes_received) < size:
+        chunk = socket.recv(size - len(bytes_received))
+        if not chunk:
+            raise ConnectionError("Socket connection closed before receiving all data")
+        bytes_received += chunk
+    return bytes_received
 
 
 def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+    #  this method continues to send data from bytes until either all data has been sent or an error occurs
+    return socket.sendall(bytes)
