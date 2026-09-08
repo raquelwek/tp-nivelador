8be26ca4c35c42a84c5b8d3d36f41390a62510e1
raquelwek <radavila@fi.uba.ar>
2026-09-08 09:04:03 -0300

    doc: para rwlock

--- services/server/src/server/rw_lock.py
+++ services/server/src/server/rw_lock.py
@@ -7,30 +7,49 @@ class RWLock:
         self.condition = threading.Condition(self.lock)
         self.readers = 0
         self.writing = False
-
+    '''
+    Adquiere el lock de lectura. Si hay un escritor activo, espera de forma
+    bloqueante hasta que este finalice. Una vez adquirido, incrementa el
+    contador de lectores.
+    '''
     def acquire_read(self):
         with self.lock:
             while self.writing:
                 self.condition.wait()
             self.readers += 1
-
+    '''
+    Libera el acceso de lectura decrementando el contador de lectores.
+    Si no quedan lectores activos, notifica a los threads que están
+    esperando para adquirir el lock.
+    '''
     def release_read(self):
         with self.lock:
             self.readers -= 1
             if self.readers == 0:
                 self.condition.notify_all()
 
+    '''
+    Adquiere el lock de escritura. Espera de forma bloqueante mientras
+    haya otro escritor activo o lectores activos. De esta forma, cuando
+    adquiere el lock, el thread es el único que puede acceder al archivo,
+    garantizando la exclusión mutua durante la escritura.
+    '''
     def acquire_write(self):
         with self.lock:
             while self.writing or self.readers > 0:
                 self.condition.wait()
             self.writing = True
-
+    '''
+    Libera el lock de escritura y notifica a los threads que están
+    esperando para adquirir el lock, permitiendo que vuelvan a evaluar
+    si pueden acceder al archivo.
+    '''
     def release_write(self):
         with self.lock:
             self.writing = False
             self.condition.notify_all()
 
+## -- funciones para adquirir el lock usando with , e.g: with <read_lock>: 
     @contextmanager
     def read_lock(self):
         self.acquire_read()
