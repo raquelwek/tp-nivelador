0fbf85e22b64841d07a9d5a9310fef1be4161200
raquelwek <radavila@fi.uba.ar>
2026-09-09 08:55:09 -0300

    fix: sacarlib de csv y procesar los datos sin la lib

--- services/client/src/model/agencyImpl.go
+++ services/client/src/model/agencyImpl.go
@@ -1,9 +1,8 @@
 package model
 
 import (
-	"encoding/csv"
+	"bufio"
 	"fmt"
-	"io"
 	"iter"
 	"os"
 	"strconv"
@@ -24,6 +23,45 @@ func CreateAgency(id int, outputFile string, inputFile string) *AgencyImpl {
 	}
 }
 
+func parseBetLine(line string, agencyID int) (Bet, error) {
+	fields := strings.Split(line, ",")
+	if len(fields) != 5 {
+		return Bet{}, fmt.Errorf("invalid bet line: expected 5 fields, got %d", len(fields))
+	}
+
+	document, err := strconv.Atoi(fields[2])
+	if err != nil {
+		return Bet{}, fmt.Errorf("invalid document %q: %w", fields[2], err)
+	}
+
+	number, err := strconv.Atoi(fields[4])
+	if err != nil {
+		return Bet{}, fmt.Errorf("invalid number %q: %w", fields[4], err)
+	}
+
+	return Bet{
+		AgencyId:  agencyID,
+		FirstName: fields[0],
+		LastName:  fields[1],
+		Document:  document,
+		Birthdate: strings.ReplaceAll(fields[3], "-", ""),
+		Number:    number,
+	}, nil
+}
+
+func formatBetLine(bet Bet) string {
+	birthdate := bet.Birthdate
+	birthdate = birthdate[0:4] + "-" + birthdate[4:6] + "-" + birthdate[6:8]
+
+	return strings.Join([]string{
+		bet.FirstName,
+		bet.LastName,
+		strconv.Itoa(bet.Document),
+		birthdate,
+		strconv.Itoa(bet.Number),
+	}, ",")
+}
+
 func (a *AgencyImpl) LoadBets() iter.Seq2[Bet, error] {
 	return func(yield func(Bet, error) bool) {
 		file, err := os.Open(a.InputFile)
@@ -33,36 +71,22 @@ func (a *AgencyImpl) LoadBets() iter.Seq2[Bet, error] {
 		}
 		defer file.Close() // Se cierra automáticamente al terminar de iterar
 
-		reader := csv.NewReader(file)
-
-		for {
-			row, err := reader.Read()
-			if err == io.EOF {
-				break
-			}
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			bet, err := parseBetLine(scanner.Text(), a.Id)
 			if err != nil {
 				yield(Bet{}, err)
 				return
-			}
-
-			name, lastName, document, birthdate, number := row[0], row[1], row[2], row[3], row[4]
-			docu, _ := strconv.Atoi(document)
-			num, _ := strconv.Atoi(number)
-			birthdate = strings.ReplaceAll(birthdate, "-", "")
-
-			bet := Bet{
-				AgencyId:  a.Id,
-				FirstName: name,
-				LastName:  lastName,
-				Document:  docu,
-				Birthdate: birthdate,
-				Number:    num,
 			}
 
 			// yield envía el valor. Si el consumidor hace un "break", yield devuelve false y salimos.
 			if !yield(bet, nil) {
 				return
 			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			yield(Bet{}, err)
 		}
 	}
 }
@@ -74,24 +98,8 @@ func (a *AgencyImpl) StoreWinner(winningBets []Bet) error {
 	}
 	defer file.Close()
 
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
 	for _, bet := range winningBets {
-		birthdate := bet.Birthdate
-
-		birthdate = birthdate[0:4] + "-" + birthdate[4:6] + "-" + birthdate[6:8]
-
-		row := []string{
-			bet.FirstName,
-			bet.LastName,
-			strconv.Itoa(bet.Document),
-			birthdate,
-			strconv.Itoa(bet.Number),
-		}
-
-		err := writer.Write(row)
-		if err != nil {
+		if _, err := file.WriteString(formatBetLine(bet) + "\n"); err != nil {
 			return fmt.Errorf("error writing to output file: %v", err)
 		}
 	}
